9fdce5897b7a9dbf03775af2c5d5955438caf590
VolodymyrV <punt_damps.0k@icloud.com>
2026-08-25 00:15:25 +0200

    chore: change Chapter 2 title to 'Meeting in'

--- js/storyData.js
+++ js/storyData.js
@@ -139,7 +139,7 @@ const CHAPTERS = [
   {
     id: 'heidelberg',
     num: 2,
-    title: 'First Meeting',
+    title: 'Meeting in',
     ico: '🏰',
     loc: 'Heidelberg',
     comic: 'assets/comic_heidelberg.jpg',
